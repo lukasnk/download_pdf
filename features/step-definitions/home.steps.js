a1a34d9223f8a5b1a7acad127c12208890ecd516
Łukasz <lukasznaurecki@gmail.com>
2024-03-29 13:19:05 +0100

    Implemented home page pattern

--- features/step-definitions/home.steps.js
+++ features/step-definitions/home.steps.js
@@ -3,16 +3,23 @@ const { Given, Then, When } = require('@cucumber/cucumber');
 const HomePage = require('../pageobjects/home.page');
 
 Given('I open the application\'s homepage', async function () {
+    await HomePage.open();
 });
 
 Then('the homepage should be loaded properly', async function () {
+    await (await HomePage.headerText).waitForDisplayed()
+    await expect(HomePage.headerText).toHaveText(process.env.APP_NAME)
 });
 
 When('I enter the product number {string} and the serial number {string}', async function (productNumber, serialNumber) {
+    productNumber = process.env.PRODUCT_NUMBER
+    serialNumber = process.env.SERIAL_NUMBER
+
+    await (await HomePage.productNumberInput).setValue(productNumber)
+    await (await HomePage.serialNumberInput).setValue(serialNumber)
 });
 
 When('I click the {string} button', async function (buttonName) {
+    await HomePage.searchButton.click()
 });
 
-Then('a new page with product information should be displayed', async function () {
-});
